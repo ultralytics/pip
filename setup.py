991c74f9bad8ab2f063fb4a0d19fbc528960896a
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-02-10 20:13:34 -0800

    Update use case

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='sampleproject_yolov5',  # Required
+    name='yolov5test2',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
--- src/sample/__init__.py
+++ src/sample/__init__.py
@@ -1,3 +1,6 @@
+# from sample import simple
+# simple.add_one(3)
+
 def main():
     """Entry point for the application script"""
     print("Call your main application code here")
