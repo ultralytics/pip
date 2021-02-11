a06d49bf1506aac4e4174c5baa456e2799831167
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-02-10 19:59:02 -0800

    Update name

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='sampleproject-yolov5',  # Required
+    name='sampleproject_yolov5',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
