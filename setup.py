cbebbd074e2dc81a1c1ed0d345f8af112863a9e9
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-02-16 21:00:28 -0800

    Update ultralytics

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(here / 'requirements.txt') as fp:  # read requirements.txt
 setup(
     name='ultralytics',  # Required https://packaging.python.org/specifications/core-metadata/#name
     version='0.0.1',  # Required https://packaging.python.org/en/latest/single_source_version.html
-    description='YOLOv5 by Ultralytics',  # Optional
+    description='Ultralytics YOLOv5 Python package, www.ultralytics.com',  # Optional
     long_description=long_description,  # Optional
     long_description_content_type='text/markdown',  # Optional
     url='https://github.com/ultralytics/pip',  # Optional, project's main homepage
